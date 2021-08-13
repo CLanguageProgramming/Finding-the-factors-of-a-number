2a6933d4631a605fc77dad3b95291769de978f4a
Skanda2007 <82853099+Skanda2007@users.noreply.github.com>
2021-08-13 13:23:18 +0530

    Update Main.c

--- Main.c
+++ Main.c
@@ -7,18 +7,27 @@ int main()
     int factor=1;
     printf("Enter a number >>");
     scanf("%d", &num);
-    while(factor <= num)
+    for(int i=1;i<=num;i++)
     {
-        if(num%factor == 0)
+        if(num%i == 0)
         {
-            printf("%d is one of the factor of %d\n", factor, num);
+            printf("%d is one of the factor of %d\n", i, num);
             factor++;
         }
         else
         {
-            factor++;
             continue;
         }
+    }
+    factor--;
+    printf("%d has %d factors\n", num, factor);
+    if(factor == 2)
+    {
+        printf("%d is a Prime Number\n", num);
+    }
+    else
+    {
+        printf("%d is a Composite Number\n", num);
     }
     printf("Thank you");
     getchar();
